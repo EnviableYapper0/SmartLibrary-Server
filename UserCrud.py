dbf977a47f8471a6c59eb63cd6bac64a784ccfb4
Laizen Paptiti <59090008@kmitl.ac.th>
2018-04-16 14:19:27 +0700

    Initial Implementation of Users Cruds and User Manager

---
+++ UserCrud.py
@@ -0,0 +1,38 @@
+from flask_restful import Resource, fields, marshal_with
+from flask_restful.reqparse import request
+
+from UserManager import UserManager
+
+book_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'registered_on': fields.DateTime(dt_format='rfc822'),
+    'is_active': fields.Boolean,
+}
+
+
+class AbstractUserCurd(Resource):
+    def __init__(self):
+        self.user_manager = UserManager()
+
+
+class BookListCurd(AbstractUserCurd):
+    @marshal_with(book_fields, envelope='resource')
+    def get(self):
+        return self.user_manager.get_all_user()
+
+    @marshal_with(book_fields, envelope='resource')
+    def post(self):
+        args = request.get_json()
+        return self.user_manager.register_new_user(args), 201
+
+
+class BookCurd(AbstractUserCurd):
+    @marshal_with(book_fields, envelope='resource')
+    def get(self, user_id):
+        return self.user_manager.get_specific_user(user_id)
+
+    @marshal_with(book_fields, envelope='resource')
+    def put(self, user_id):
+        args = request.get_json()
+        return self.user_manager.update_user_data(user_id, args)
---
+++ UserManager.py
@@ -0,0 +1,24 @@
+from model import User
+
+
+class UserManager(object):
+    def get_all_user(self):
+        user_list = []
+
+        for user in User.select():
+            user_list.append(user_list)
+
+        return user_list
+
+    def register_new_user(self, json_data):
+        return User.create(**json_data)
+
+    def get_specific_user(self, user_id):
+        return User.get_by_id(user_id)
+
+    def update_user_data(self, user_id, json_data):
+        if 'id' in json_data and json_data['id'] != user_id:
+            raise IndexError("Id mismatched.")
+
+        User.update(**json_data).where(User.id == user_id)
+        return self.get_specific_user(user_id)
