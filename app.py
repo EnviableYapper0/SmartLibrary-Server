14fced9b7130c80464c9196f9e9bd962f46950eb
EnviableYapper0 <EnviableYapper0@outlook.com>
2018-04-21 10:21:41 +0700

    Add bad input and index error handler

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from peewee import IntegrityError
+
+from BookCirculationCrud import BorrowApi, ReturnApi
 from BookCrud import BookListCurd, BookCurd, BookRfidCurd
 from UserCrud import UserListCurd, UserCurd, UserRfidCurd
-from BookCirculationCrud import BorrowApi, ReturnApi
-from model import Book, User, BookCirculation
+import error_handler
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,6 +24,9 @@ api.add_resource(UserCurd, '/user/<user_id>')
 api.add_resource(UserRfidCurd, '/user/rfid/<rfid>')
 api.add_resource(BorrowApi, '/borrow')
 api.add_resource(ReturnApi, '/return/<borrow_id>')
+
+app.register_error_handler(IntegrityError, error_handler.bad_input_handler)
+app.register_error_handler(IndexError, error_handler.index_error_handler)
 
 if __name__ == '__main__':
     app.run()
